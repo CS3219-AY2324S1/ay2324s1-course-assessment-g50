29ca08e830b665b3df30e6264b15f3f525f860bb
-Unique° <jsjt2p@gmail.com>
2023-10-07 19:05:52 +0800

    update constant

--- api/src/db/constant/userConstant.js
+++ api/src/db/constant/userConstant.js
@@ -3,5 +3,5 @@ module.exports = {
     DEFAULT_BIRTH: new Date('1970-01-01'),
     DEFAULT_SIGN: 'Lets prepare coding question together~',
     DEFAULT_GENDER: 'Unknown',
-    DEFAULT_AVATOR: 'https://ibb.co/HC86ZDj',
+    DEFAULT_AVATOR: 'https://i.ibb.co/gdKNYMx/1683184422648379.jpg',
 }
