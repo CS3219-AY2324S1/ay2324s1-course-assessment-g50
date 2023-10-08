9abc932ddf89981137abf89b6294d3e26de4e32c
LJXSean <risingsun221@gmail.com>
2023-10-09 03:27:46 +0800

    Update routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Alerts from "./components/alerts/alerts";
 import Question from "./components/questions/questions";
 import UserProfileManager from "./components/user/UserAuthentication";
 import Profile from "./components/user/Profile";
+import PrivateRoute from "./components/routing/privateRoute";
 import "./App.css";
 
 /* To enable sending of cookies on each request globally*/
@@ -12,23 +13,13 @@ axios.defaults.withCredentials = true;
 
 const App = () => (
   <Router>
+    <Alerts />
     <Routes>
-      <Route path="/" element={<QuestionPage />} />
-      
-      <Route>
-        <Route path="/login" element={<UserProfileManager />}/>
-        <Route path="/profile" element={<Profile />}/>
-      </Route>
-
+      <Route path="/" element={<PrivateRoute component={Question}/>} />
+      <Route path="/login" element={<UserProfileManager />} />
+      <Route path="/profile" element={<PrivateRoute component={Profile}/>} />
     </Routes>
   </Router>
-);
-
-const QuestionPage = () => (
-  <div>
-    <Question />
-    <Alerts />
-  </div>
 );
 
 export default App;
