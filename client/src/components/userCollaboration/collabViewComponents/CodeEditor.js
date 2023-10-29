cf16426f3bcbfcebf824e0e1a1f33a66d75f4af7
Ong Wei Jie <ongweijie7@gmail.com>
2023-10-29 18:16:10 +0800

    Refactor collaboration page to handle smaller windows and also move logic of changing languages to parent component of CollabView.

--- client/src/components/userCollaboration/collabViewComponents/CodeEditor.js
+++ client/src/components/userCollaboration/collabViewComponents/CodeEditor.js
@@ -1,14 +1,7 @@
 import { useState, useRef, useEffect } from "react";
 import "./codeEditor.css";
 import Console from "./Console";
-import * as Y from 'yjs';
-import { WebsocketProvider } from 'y-websocket';
-import { MonacoBinding } from 'y-monaco'
 import Editor, { DiffEditor, useMonaco, loader } from '@monaco-editor/react';
-import InfoBar from "./InfoBar";
-import { useSelector } from "react-redux";
-
-const serverWsUrl = "ws://localhost:8200";
 
 const languageDict = {
     "java": "//",
@@ -16,13 +9,9 @@ const languageDict = {
     "javascript": "//"
 };
 
-const CodeEditor = () => {
-    const matchInfo = useSelector(state => state.matching);
-    const doc = new Y.Doc();
+const CodeEditor = ({ language, handleEditorDidMount }) => {
     const editorRef = useRef();
 
-
-    const [language, setLanguage] = useState("python");
     const [isShowConsole, setIsShowConsole] = useState(false);
 
     // Handle Console state change
@@ -30,69 +19,30 @@ const CodeEditor = () => {
         setIsShowConsole(!isShowConsole);
     }
 
-    // Handle editor code change
-    const handleEditorDidMount = (editor, monaco) => {
-        editorRef.current = editor;
-
-        // Code Collaboration part:
-        const manacoText = doc.getText("manaco")
-        const provider = new WebsocketProvider(serverWsUrl, "matchId", doc);
-        const binding = new MonacoBinding(manacoText, editorRef.current.getModel(), new Set([editorRef.current]))
-    }
-
     // Handle editor code submission
     const handleSubmitCode = () => {
         console.log(editorRef.current.getValue());
     }
 
-
-    // Handle language change
-    useEffect(() => {
-        const languageText = doc.getText("language")
-
-        languageText.observe(event => {
-            setLanguage(languageText.toString());
-        });
-    }, [])
-
-    const handleLanguageChange = (event) => {
-        const newLanguage = event.target.value
-
-        // Language Synchronize part:
-        const languageText = doc.getText("language")
-        const provider = new WebsocketProvider(serverWsUrl, "matchId", doc);
-        languageText.delete(0, languageText.length);
-        languageText.insert(0, newLanguage);
-    }
-
     return (
-        <div className="code-editor-container">
-            <div className="code-container">
-                <div className="editor-container">
-                    <Editor className="editor" height="99%" defaultLanguage="python"
-                        defaultValue={`#Type your code here`}
-                        language={language}
-                        onMount={handleEditorDidMount}
-                        options={{
-                            scrollBeyondLastLine: false,
-                            fontSize: "14px",
-                            minimap: {
-                                enabled: false,
-                            }
-                        }} />
-                </div>
-                <div className={isShowConsole ? 'console-result visible' : 'console-result'}>
-                    <p>Result:</p>
-                </div>
-                <Console handleSubmitCode={handleSubmitCode} handleShowConsole={handleShowConsole} />
+        <div className="code-container">
+            <div className="editor-container">
+                <Editor className="editor" height="99%" defaultLanguage="python"
+                    defaultValue={`#Type your code here`}
+                    language={language}
+                    onMount={handleEditorDidMount}
+                    options={{
+                        scrollBeyondLastLine: false,
+                        fontSize: "14px",
+                        minimap: {
+                            enabled: false,
+                        }
+                    }} />
             </div>
-            <div className="editor-info-container">
-                <InfoBar
-                    matchInfo={matchInfo}
-                    selectedLanguage={language}
-                    handleLanguageChange={handleLanguageChange}
-                />
+            <div className={isShowConsole ? 'console-result visible' : 'console-result'}>
+                <p>Result:</p>
             </div>
+            <Console handleSubmitCode={handleSubmitCode} handleShowConsole={handleShowConsole} />
         </div>
     );
 }
--- client/src/components/userCollaboration/viewComponents/CollabView.js
+++ client/src/components/userCollaboration/viewComponents/CollabView.js
@@ -1,18 +1,64 @@
 import "./collabView.css";
 import CodeEditor from "../collabViewComponents/CodeEditor";
+import InfoBar from "../collabViewComponents/InfoBar";
 import { retrieveQuestionDetailsAction } from "../../../reducers/matchingSlice";
 import { fetchQuestions } from "../../../reducers/questionSlice";
-import { useEffect } from "react";
+import { useEffect, useState, useRef } from "react";
+import { WebsocketProvider } from 'y-websocket';
+import { MonacoBinding } from 'y-monaco'
 import { useDispatch, useSelector } from "react-redux";
 import { BsArrowLeftSquareFill } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
+import * as Y from 'yjs';
 
-/* Component which shows the question details */
+const serverWsUrl = "ws://localhost:8200";
+
+/* Component which shows the collaboration view */
 const CollabView = () => {
     const questionArr = useSelector(state => state.questions.questions);
     const question = useSelector(state => state.matching.matchedQuestionDetails);
+    
     const dispatch = useDispatch();
     const navigate = useNavigate();
+
+    /* Props info for CodeEditor & InfoBar */
+    const [language, setLanguage] = useState("python");
+    const matchInfo = useSelector(state => state.matching);
+
+    const doc = new Y.Doc();
+    const editorRef = useRef();
+
+    // Handle language change
+    useEffect(() => {
+
+    }, [language]);
+
+    // Handle editor code change
+    const handleEditorDidMount = (editor, monaco) => {
+        editorRef.current = editor;
+
+        // Code Collaboration part:
+        const manacoText = doc.getText("manaco")
+        const provider = new WebsocketProvider(serverWsUrl, "matchId", doc);
+        const binding = new MonacoBinding(manacoText, editorRef.current.getModel(), new Set([editorRef.current]))
+    }
+
+    const handleLanguageChange = (event) => {
+        const newLanguage = event.target.value
+
+        // Language Synchronize part:
+        const languageText = doc.getText("language")
+        const provider = new WebsocketProvider(serverWsUrl, "matchId", doc);
+        languageText.delete(0, languageText.length);
+        languageText.insert(0, newLanguage);
+
+        // Handle language change
+        const updatedLanguageText = doc.getText("language")
+        updatedLanguageText.observe(event => {
+            setLanguage(languageText.toString());
+        });
+    }
+    /* ********************************************* */
 
     const goBack = () => {
         navigate('/');
@@ -31,7 +77,7 @@ const CollabView = () => {
     }, [questionArr])
 
     return (
-        <div className="question-details-component">
+        <div className="collab-view">
             {question && 
             <div className="question-details">
                 <p className="question-title">{question.title}</p>
@@ -39,8 +85,10 @@ const CollabView = () => {
                 <div  dangerouslySetInnerHTML={{ __html: question.description }} />
                 <BsArrowLeftSquareFill onClick={() => goBack()} className="return-icon"/>
             </div>}
-            <CodeEditor/>
 
+            <CodeEditor handleEditorDidMount={handleEditorDidMount} language={language}/>
+
+            <InfoBar matchInfo={matchInfo} selectedLanguage={language} handleLanguageChange={handleLanguageChange}/>
         </div>
     )
 }
