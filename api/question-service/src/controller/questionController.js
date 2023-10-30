7f0fc33203cd60431e0f53f1e341d25a918e182a
-Unique° <jsjt2p@gmail.com>
2023-10-30 12:02:08 +0800

    Implement enhance question backend
    Use GET /questions api.
    1. Search question title by keyword
    2. Filter question by tags
    3. Pagination
    4. Sort question by certain keys

--- api/question-service/src/controller/questionController.js
+++ api/question-service/src/controller/questionController.js
@@ -1,28 +1,28 @@
-const JsonResponse = require('../common/jsonResponse')
-const question = require('../db/models/question')
+const JsonResponse = require('../common/jsonResponse');
+const question = require('../db/models/question');
 
 // --------------------------------------- Service logic -------------------------------------------
 
 // Add question to repo
 async function addQuestion(req, res) {
     // Create a question model
-    const { title, description, categories, complexity } = req.body
-    const newQuestion = new question({ title, description, categories, complexity })
+    const { title, description, categories, complexity } = req.body;
+    const newQuestion = new question({ title, description, categories, complexity });
 
-    // If question title already exisits, return fail
-    const dbQuestion = await question.find({title: title}).catch(err => {
-        return JsonResponse.fail(500, err).send(res)
-    })
+    // If question title already exists, return fail
+    const dbQuestion = await question.find({ title: title }).catch(err => {
+        return JsonResponse.fail(500, err).send(res);
+    });
     if (dbQuestion.length > 0) {
-        return JsonResponse.fail(400, 'This question already exisits in repo').send(res)
+        return JsonResponse.fail(400, 'This question already exists in repo').send(res);
     }
 
-    // Save model to DB:
+    // Save model to DB
     await newQuestion.save().then(savedQuestion => {
-        return JsonResponse.success(201, savedQuestion).send(res)
+        return JsonResponse.success(201, savedQuestion).send(res);
     }).catch(err => {
-        return JsonResponse.fail(500, err).send(res)
-    })
+        return JsonResponse.fail(500, err).send(res);
+    });
 }
 
 // Get a question details including examples and test cases
@@ -33,74 +33,102 @@ async function getQuestionByID(req, res) {
         return JsonResponse.success(200, question).send(res);
     }).catch(err => {
         return JsonResponse.fail(500, err).send(res);
-    })
+    });
 }
 
 // Get questions by filter
 async function getQuestions(req, res) {
-    // Get filtered info:
-    const { title, description, categories, complexity } = req.query
-    // Filer constructor:
-    const filter = {}
-    if (title) {
-        filter.title = title
+    // Get Filter, sort and page info
+    const { keyword, topicSlugs, complexity, sort, page, pageSize } = req.query;
+
+    /* Filter constructor */
+    const filter = {};
+    // Build keyword filter
+    if (keyword) {
+        const regex = new RegExp(keyword, 'i');
+        filter.$or = [
+            { title: { $regex: regex } },
+        ];
     }
-    if (description) {
-        filter.description = description
+    // Build topic filter
+    if (topicSlugs) {
+        const topicArray = decodeURIComponent(topicSlugs).split(',')
+        filter.categories = { $all: topicArray };
     }
-    if (categories) {
-        filter.categories = categories;
-    }
+    // Build Complexity Filter
     if (complexity) {
         filter.complexity = complexity;
     }
-    // Get target questions by filtering options:
-    await question.find(filter).then(targetQuestions => {
-        return JsonResponse.success(200, targetQuestions).send(res)
-    }).catch(err => {
-        return JsonResponse.fail(500, err).send(res)
-    })
+
+    /* Sorting constructor */
+    const sortOptions = {};
+    if (sort) {
+        if (sort.startsWith('-')) {
+            // Sort in descending order
+            sortOptions[sort.substring(1)] = -1;
+        } else {
+            // Sort in ascending order
+            sortOptions[sort] = 1;
+        }
+    }
+
+    /* Calculate pagination options */
+    const pageNumber = parseInt(page) || 1; // Default to page 1
+    const itemsPerPage = parseInt(pageSize) || 10; // Default to 10 items per page
+    const skip = (pageNumber - 1) * itemsPerPage;
+
+    /* Get target questions by filtering options, pagination, and sorting logic */
+    await question.find(filter)
+        .sort(sortOptions)
+        .skip(skip)
+        .limit(itemsPerPage)
+        .then(targetQuestions => {
+            return JsonResponse.success(200, targetQuestions).send(res);
+        })
+        .catch(err => {
+            return JsonResponse.fail(500, err).send(res);
+        });
 }
 
 // Update question
 async function updateQuestion(req, res) {
     // Get target update info
-    const { title, description, categories, complexity } = req.body
-    const id = req.params.id
+    const { title, description, categories, complexity } = req.body;
+    const id = req.params.id;
 
-    // If same question title as question of different id, return fail
-    const dbQuestion = await question.find({title}).catch(err => {
-        return JsonResponse.fail(500, err).send(res)
-    })
-    const duplicates = dbQuestion.filter(q => q._id.valueOf() !== id)
+    // If the same question title as a question of a different id, return fail
+    const dbQuestion = await question.find({ title }).catch(err => {
+        return JsonResponse.fail(500, err).send(res);
+    });
+    const duplicates = dbQuestion.filter(q => q._id.valueOf() !== id);
     if (duplicates.length > 0) {
-        return JsonResponse.fail(400, 'Another question of the same title already exists').send(res)
+        return JsonResponse.fail(400, 'Another question of the same title already exists').send(res);
     }
 
     // Update question by Id
     await question.findByIdAndUpdate(id, { title, description, categories, complexity }, { new: true }).then(updatedQuestion => {
         if (!updatedQuestion) {
-            return JsonResponse.fail(404, 'Question not found').send(res)
+            return JsonResponse.fail(404, 'Question not found').send(res);
         }
-        return JsonResponse.success(201, updatedQuestion).send(res)
+        return JsonResponse.success(201, updatedQuestion).send(res);
     }).catch(err => {
-        return JsonResponse.fail(500, err).send(res)
-    })
+        return JsonResponse.fail(500, err).send(res);
+    });
 }
 
 // Delete question from repo
 async function deleteQuestion(req, res) {
     // Get target question info
-    const id = req.params.id
+    const id = req.params.id;
     // Delete question by id
     await question.findByIdAndDelete(id).then(deletedQuestion => {
         if (!deletedQuestion) {
-            return JsonResponse.fail(404, 'Question not found').send(res)
+            return JsonResponse.fail(404, 'Question not found').send(res);
         }
-        return JsonResponse.success(204, 'Deletion Successful').send(res)
+        return JsonResponse.success(204, 'Deletion Successful').send(res);
     }).catch(err => {
-        return JsonResponse.fail(500, err).send(res)
-    })
+        return JsonResponse.fail(500, err).send(res);
+    });
 }
 
-module.exports = { addQuestion, getQuestions, updateQuestion, deleteQuestion, getQuestionByID }
+module.exports = { addQuestion, getQuestions, updateQuestion, deleteQuestion, getQuestionByID };
