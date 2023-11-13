7db5cfdeefc13044c9fe8cce2c38949b7c23928f
xpzmichael <xpzmichael@gmail.com>
2023-11-13 21:33:18 +0800

    fix

--- client/src/urls.js
+++ client/src/urls.js
@@ -1,8 +1,8 @@
-export const chatbaseUrl = "http://34.143.231.112/api-gateway/chat";
-export const matchingBaseUrl = "http://34.143.231.112/api-gateway/matching";
-export const questionBaseUrl = "http://34.143.231.112/api-gateway/questions";
-export const sandboxBaseUrl = "http://34.143.231.112/api-gateway/sandbox";
-export const userBaseUrl = 'http://34.143.231.112/user-service';
+export const chatbaseUrl = "http://10.148.0.2/api-gateway/chat";
+export const matchingBaseUrl = "http://10.148.0.2/api-gateway/matching";
+export const questionBaseUrl = "http://10.148.0.2/api-gateway/questions";
+export const sandboxBaseUrl = "http://10.148.0.2/api-gateway/sandbox";
+export const userBaseUrl = 'http://10.148.0.2/user-service';
 
-export const serverWsUrl = "ws://34.143.231.112/collaboration-socket";
-export const communicationSocketUrl = "ws://34.143.231.112/communication-socket";
+export const serverWsUrl = "ws://10.148.0.2/collaboration-socket";
+export const communicationSocketUrl = "ws://10.148.0.2/communication-socket";
