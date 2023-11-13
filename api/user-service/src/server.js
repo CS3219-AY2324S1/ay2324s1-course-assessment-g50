2c568c3c1521dd257a742c671ecff5b0d58bde0c
xpzmichael <xpzmichael@gmail.com>
2023-11-14 01:20:04 +0800

    test user service

--- api/user-service/src/server.js
+++ api/user-service/src/server.js
@@ -1,33 +1,37 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors'); // Add this line
 
 // Middlewares
-app.use(express.json())
+app.use(express.json());
 const session = require('express-session');
 const sessionConfig = require('./configs/sessionConfigs');
-app.use(session(sessionConfig))
+app.use(session(sessionConfig));
 
-// Routes:
+// Enable CORS for all routes
+app.use(cors());
+
+// Routes
 const routes = require('./routes/userRoutes');
-app.use('/users', routes)
+app.use('/users', routes);
 
-// DB:
+// DB
 const mySQLDB = require('./db/mysql/mysql');
 
 // Start the server
 async function startServer() {
   try {
-    await mySQLDB.sequelize.sync()
-    console.log("MySQL Initialized successfully")
+    await mySQLDB.sequelize.sync();
+    console.log("MySQL Initialized successfully");
 
-    const PORT = process.env.PORT
+    const PORT = process.env.PORT;
     app.listen(PORT, () => {
-      console.log(`User server started on ${PORT}`)
-    })
+      console.log(`User server started on ${PORT}`);
+    });
   } catch (error) {
-    console.error('Error starting user server: ', error)
+    console.error('Error starting user server: ', error);
   }
 }
 
-startServer()
+startServer();
