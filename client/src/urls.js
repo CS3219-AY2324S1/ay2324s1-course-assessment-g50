fbe9b229c01e48abb129436661b990713586541c
xpzmichael <xpzmichael@gmail.com>
2023-11-14 21:48:31 +0800

    Update urls.js

--- client/src/urls.js
+++ client/src/urls.js
@@ -2,7 +2,7 @@ export const chatbaseUrl = "http://35.198.214.47/api-gateway/chat";
 export const matchingBaseUrl = "http://35.198.214.47/api-gateway/matching";
 export const questionBaseUrl = "http://35.198.214.47/api-gateway/questions";
 export const sandboxBaseUrl = "http://35.198.214.47/api-gateway/sandbox";
-export const userBaseUrl = 'http://35.198.214.47/api_gateway';
+export const userBaseUrl = 'http://35.198.214.47/api-gateway';
 
 export const serverWsUrl = "ws://35.198.214.47/collaboration-socket";
 export const communicationSocketUrl = "ws://35.198.214.47/communication-socket";
