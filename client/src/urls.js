4aa495a0248d6658b5a0d47c1204b78e8c19beb5
xpzmichael <xpzmichael@gmail.com>
2023-11-15 16:01:53 +0800

    domain

--- client/src/urls.js
+++ client/src/urls.js
@@ -1,8 +1,8 @@
-export const chatbaseUrl = "http://peerprepg50:5000/chat";
-export const matchingBaseUrl = "http://peerprepg50:5000/matching";
-export const questionBaseUrl = "http://peerprepg50:5000/questions";
-export const sandboxBaseUrl = "http://peerprepg50:5000/sandbox";
-export const userBaseUrl = 'http://peerprepg50:5000/users';
+export const chatbaseUrl = "http://peerprepg50.site:5000/chat";
+export const matchingBaseUrl = "http://peerprepg50.site:5000/matching";
+export const questionBaseUrl = "http://peerprepg50.site:5000/questions";
+export const sandboxBaseUrl = "http://peerprepg50.site:5000/sandbox";
+export const userBaseUrl = 'http://peerprepg50.site:5000/users';
 
-export const serverWsUrl = "ws://peerprepg50:5000/collaboration";
-export const communicationSocketUrl = "ws://peerprepg50:5000/";
+export const serverWsUrl = "ws://peerprepg50.site:5000/collaboration";
+export const communicationSocketUrl = "ws://peerprepg50.site:5000/";
