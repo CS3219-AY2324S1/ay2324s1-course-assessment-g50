4290877ae9ff009e854aaeccf97ba224300cd471
xpzmichael <xpzmichael@gmail.com>
2023-11-15 16:00:38 +0800

    try domain

--- client/src/urls.js
+++ client/src/urls.js
@@ -1,8 +1,8 @@
-export const chatbaseUrl = "http://35.198.214.47:5000/chat";
-export const matchingBaseUrl = "http://35.198.214.47:5000/matching";
-export const questionBaseUrl = "http://35.198.214.47:5000/questions";
-export const sandboxBaseUrl = "http://35.198.214.47:5000/sandbox";
-export const userBaseUrl = 'http://35.198.214.47:5000/users';
+export const chatbaseUrl = "http://peerprepg50:5000/chat";
+export const matchingBaseUrl = "http://peerprepg50:5000/matching";
+export const questionBaseUrl = "http://peerprepg50:5000/questions";
+export const sandboxBaseUrl = "http://peerprepg50:5000/sandbox";
+export const userBaseUrl = 'http://peerprepg50:5000/users';
 
-export const serverWsUrl = "ws://35.198.214.47:5000/collaboration";
-export const communicationSocketUrl = "ws://35.198.214.47:5000/";
+export const serverWsUrl = "ws://peerprepg50:5000/collaboration";
+export const communicationSocketUrl = "ws://peerprepg50:5000/";
