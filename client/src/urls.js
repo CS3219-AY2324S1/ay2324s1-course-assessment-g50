4f7a8cdb10478502452932e47cba9d950453c7a9
xpzmichael <xpzmichael@gmail.com>
2023-11-15 14:02:14 +0800

    Update urls.js

--- client/src/urls.js
+++ client/src/urls.js
@@ -2,7 +2,7 @@ export const chatbaseUrl = "http://peerprepg50.site/api-gateway/chat";
 export const matchingBaseUrl = "http://peerprepg50.site/api-gateway/matching";
 export const questionBaseUrl = "http://peerprepg50.site/api-gateway/questions";
 export const sandboxBaseUrl = "http://peerprepg50.site/api-gateway/sandbox";
-export const userBaseUrl = 'http://peerprepg50.site/api-gateway';
+export const userBaseUrl = 'http://peerprepg50.site/api-gateway/users';
 
 export const serverWsUrl = "ws://peerprepg50.site/collaboration-socket";
 export const communicationSocketUrl = "ws://peerprepg50.site/communication-socket";
