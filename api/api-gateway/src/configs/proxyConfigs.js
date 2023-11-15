59edfc53d0465db05f67232689ec57d3495111a6
xpzmichael <xpzmichael@gmail.com>
2023-11-15 13:56:46 +0800

    Update proxyConfigs.js

--- api/api-gateway/src/configs/proxyConfigs.js
+++ api/api-gateway/src/configs/proxyConfigs.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 // Cors Configs
 const corsConfig = cors({
     credentials: true,
-    origin: 'http://peerprepg50.site:3000',
+    origin: process.env.FRONTEND_URL,
 });
 
 // Proxy Configs
