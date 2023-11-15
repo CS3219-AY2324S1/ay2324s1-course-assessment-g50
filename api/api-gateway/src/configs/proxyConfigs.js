5c9d3736c7f1bd46d53d535706fbc4b6f01f0d01
xpzmichael <xpzmichael@gmail.com>
2023-11-15 13:49:24 +0800

    3000

--- api/api-gateway/src/configs/proxyConfigs.js
+++ api/api-gateway/src/configs/proxyConfigs.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 // Cors Configs
 const corsConfig = cors({
     credentials: true,
-    origin: 'http://peerprepg50.site',
+    origin: 'http://peerprepg50.site:3000',
 });
 
 // Proxy Configs
